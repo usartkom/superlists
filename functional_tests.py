c3fb45221386eda09e7c69ff859431b7f1ced9eb
usartkom <artem.komarichev@wayne.edu>
2015-07-13 14:00:55 -0400

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -33,8 +33,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
